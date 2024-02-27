f9c98f97c38d55beb5dec194c4bc33091767513f
adan_due <ing.adanduenas@gmail.com>
2024-02-27 13:33:57 -0600

    Updates

---
+++ assets/main.js
@@ -0,0 +1,39 @@
+const API = 'https://youtube-v311.p.rapidapi.com/activities/?part=snippet&channelId=UCgr5q49QjDcqZrhYLQKHaTw&maxResults=5';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b2b69196femsheee69769e2f6bc2p128043jsn84cfbf9e8113',
+		'X-RapidAPI-Host': 'youtube-v311.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+        <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+            </h3>
+        </div>
+        </div>
+    `).slice(0, 4).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
