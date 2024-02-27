1409009b5380d6fccec02823a72ff3b7ca59db9b
adan_due <ing.adanduenas@gmail.com>
2024-02-27 13:33:31 -0600

    Preparando todo para el deploy

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,4 @@
-const API = 'https://youtube-v311.p.rapidapi.com/activities/?part=snippet&channelId=UCKEaZFKeCBM7JxNv192Q1Pw&maxResults=3';
-const content = null || document.getElementById('content')
+const API = 'https://youtube-v311.p.rapidapi.com/activities/?part=snippet&channelId=UCgr5q49QjDcqZrhYLQKHaTw&maxResults=5';
 const options = {
 	method: 'GET',
 	headers: {
@@ -9,30 +8,32 @@ const options = {
 };
 
 async function fetchData(urlApi) {
-    const response = await fetch(urlApi, options)
-    const data = await response.json()
-    return data
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
 }
 
 (async () => {
-    try{
-        const videos = await fetchData(API)
-        let view = `
-        ${videos.items.map(video => `
+    try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
         <div class="group relative">
-            <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
-            <div class="mt-4 flex justify-between">
-                <h3 class="text-sm text-gray-700">
-                    <span aria-hidden="true" class="absolute insert-0"></span>
-                    ${video.snippet.title}
-                </h3>
-            </div>
+        <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+            </h3>
         </div>
-        `).slice(0,4).join('')}
-        `
-    } catch{
-
-    }
+        </div>
+    `).slice(0, 4).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
 })();
